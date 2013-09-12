925df288f6b997e89e4f5f343b1f0cf042eb2883
CAA-jwu <jwu@caa.com>
2013-09-12 16:01:01 -0700

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ describe User do
 
   describe "when email format is invalid" do
   	it "should be invalid" do
-  		addresses = %w[user@foo,com user_a_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+  		addresses = %w[user@foo,com user_a_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
   		addresses.each do |invalid_address|
   			@user.email = invalid_address
   			expect(@user).not_to be_valid
@@ -61,14 +61,11 @@ describe User do
   end
 
   describe "when password is not present" do
-  	before do
-  		@user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
-  	end
-
-  	it "some string" do
-  		true.should == false
-  	end
-
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
   end
 
   describe "when password doesn't match confirmation" do
@@ -97,5 +94,14 @@ describe User do
   	end
   end
 
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
+  	end
+  end
 
 end
